c8808c7214936e2052bf7b4e1171d468535f5566
caozhix <1209970407@qq.com>
2020-01-03 02:05:01 -0500

    yixiugai

---
+++ test.c
@@ -0,0 +1,20 @@
+/*================================================================
+*   Copyright (C) 2020 hqyj Ltd. All rights reserved.
+*   
+*   文件名称：test.c
+*   创 建 者：cao
+*   创建日期：2020年01月03日
+*   描    述：
+*
+================================================================*/
+
+
+#include <stdio.h>
+
+
+int main(int argc, char *argv[])
+{
+printf("hello world\n");	
+	return 0;
+}
+
